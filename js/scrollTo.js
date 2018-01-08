330bd22e2d18b7c9f33f39ca8b18a7da9a22053d
Matt Desimini <mdesimini@yahoo.com>
2018-01-08 05:01:41 -0500

    added content, layout, responsiveness

---
+++ js/scrollTo.js
@@ -0,0 +1,23 @@
+    $("#scrollToNews").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#news").offset().top-70
+        }, 1000);
+    });  
+      
+    $("#scrollToEsports").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#vid").offset().top-70
+        }, 1000);
+    });  
+
+    $("#scrollToNewsMobile").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#news").offset().top-70
+        }, 1000);
+    });  
+      
+    $("#scrollToEsportsMobile").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#vid").offset().top-70
+        }, 1000);
+    });  
